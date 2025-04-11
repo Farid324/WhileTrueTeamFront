eb2449b90b3a3e024811d67b4fc4a22a4f78a497
Alan <ferchoalan81@gmail.com>
2025-04-11 18:20:20 -0400

    Created UserIcon

--- my-app/src/app/home/UserPerfil/page.tsx
+++ my-app/src/app/home/UserPerfil/page.tsx
@@ -2,6 +2,7 @@
 import Inputlabel from "../../components/input/Inputlabel";
 import Link from 'next/link';
 import NavbarPerfilUsuario from "@/app/components/navbar/NavbarPerfilUsuario";
+import Button from "@/app/components/botons/botons";
 
 export default function UserPerfilPage() {
   return (
@@ -9,31 +10,27 @@ export default function UserPerfilPage() {
       <NavbarPerfilUsuario />
 
       <main className="min-h-screen p-8 bg-white text-gray-900">
-        <h1 className="text-3xl font-bold mb-4">Perfil del Usuario</h1>
-        <p className="mb-6">Aquí puedes editar los datos de tu perfil.</p>
-
+        
         <form method="PUT" className="space-y-6 max-w-2xl">
           <Inputlabel id="Nombre" label="Nombre Completo" type="Text" placeholder="Example"/>
           <Inputlabel id="Email" label="Email" type="Text" placeholder="Example@gmail.com" />
           <Inputlabel id="Contraseña" label="Contraseña" type="Text" placeholder="*************" />
-          <Inputlabel id="Documento ID" label="Documendo de Identidad" type="Text" placeholder="Example" />
-          <Inputlabel id="Licencia" label="Licencia de Conducir" type="Text" placeholder="Example" />
 
+          <div className="flex flex-row">
+            <Inputlabel id="Fecha" label="Fecha de Nacimiento" type="date" placeholder="DD/MM/YY" />
+            <Inputlabel id="Telefono" label="Telefono" type="number" placeholder="3842944" /> 
+          </div>
+
+            <Inputlabel id="Documento ID" label="Documento de Identidad" type="Text" placeholder="Example" />
+            <Inputlabel id="Licencia" label="Licencia de Conducir" type="Text" placeholder="Example" />
+          
+          
           <div className="flex justify-end gap-4 pt-4">
-            <button
-              type="submit"
-              className="bg-blue-600 text-white px-6 py-2 rounded-md hover:bg-blue-700 transition-colors"
-
-            >
-              Guardar
-            </button>
-            <button
-              type="button"
-              onClick={() => window.history.back()} //Función arrow
-              className="bg-gray-300 text-gray-800 px-6 py-2 rounded-md hover:bg-gray-400 transition-colors"
-            >
-              Cancelar
-            </button>
+            
+          <Button id="Guardar Perfil" color="bg-blue-600 text-white px-6 py-2 rounded-md hover:bg-blue-700 transition-colors" type="button" Guardar="Guardar" deshabilitado={true} />
+          <Button id="Cancelar Perfil" color="bg-blue-600 text-white px-6 py-2 rounded-md hover:bg-blue-700 transition-colors" type="button" Guardar="Cancelar" deshabilitado={false} />  
+            
+           
           </div>
         </form>
 
