30f81e9b21fc9ee3722804a244146126efb81d48
Domis382 <naviaeddy@gmail.com>
2025-04-22 10:54:08 -0400

    se corrigieron bugs y añadieron validaciones, ademas del registro con google. varios de estos cambios seran eliminados para que los correspondientes desarrolladores a cargo puedan hacer sus commits

--- my-app/src/app/components/navbar/Navbar.tsx
+++ my-app/src/app/components/navbar/Navbar.tsx
@@ -44,12 +44,12 @@ export default function Navbar({ onLoginClick, onRegisterClick }: {
         </div>
 
         <div className="flex justify-center md:justify-end gap-0 w-full md:w-auto">
-          <button onClick={onRegisterClick} className="w-1/2 md:w-auto px-4 md:px-8 py-[0.4rem] rounded-l-[20px] bg-[var(--naranja-46)] font-[var(--tamaño-regular)] text-[var(--azul-oscuro)] shadow-[var(--sombra)] text-sm md:text-base">
+          <button onClick={onRegisterClick} className=" cursor-pointer w-1/2 md:w-auto px-4 md:px-8 py-[0.4rem] rounded-l-[20px] bg-[var(--naranja-46)] font-[var(--tamaño-regular)] text-[var(--azul-oscuro)] shadow-[var(--sombra)] text-sm md:text-base">
             Registrarse
           </button>
           <button
             onClick={onLoginClick}
-            className="w-1/2 md:w-auto px-4 py-[0.4rem] rounded-r-[20px] bg-[var(--naranja)] text-[var(--blanco)] font-[var(--tamaña-bold)] shadow-[var(--sombra)] transition-transform duration-100 active:scale-[0.97] active:shadow-[0_1px_3px_rgba(0,0,0,0.2)] text-sm md:text-base"
+            className="cursor-pointer w-1/2 md:w-auto px-4 py-[0.4rem] rounded-r-[20px] bg-[var(--naranja)] text-[var(--blanco)] font-[var(--tamaña-bold)] shadow-[var(--sombra)] transition-transform duration-100 active:scale-[0.97] active:shadow-[0_1px_3px_rgba(0,0,0,0.2)] text-sm md:text-base"
           >
             Iniciar Sesión
           </button>
