4378602fc4f53a2d2ab67794da09966245b181a0
Grissell Coca <grissellcadima@gmail.com>
2025-04-25 20:51:15 -0400

    Arreglo de bugs

--- my-app/src/app/home/terminos/TerminosLogin.tsx
+++ my-app/src/app/home/terminos/TerminosLogin.tsx
@@ -1,13 +1,10 @@
 'use client';
 import React, { useEffect, useState } from 'react';
-import Navbar from '@/app/components/navbar/Navbar';
-import LoginModal from '@/app/components/auth/authInicioSesion/LoginModal';
-import RegisterModal from '@/app/components/auth/authregistro/RegisterModal';
+import Navbar from '@/app/components/navbar/NavbarInicioSesion';
 
 
 export default function Terminos() {
   const [activeSection, setActiveSection] = useState<string | null>(null);
-  const [activeModal, setActiveModal] = useState<'login' | 'register' | null>(null);
 
   useEffect(() => {
     const sections = document.querySelectorAll('section[id]');
@@ -37,10 +34,6 @@ export default function Terminos() {
 
   return (
     <div className="flex flex-col min-h-screen bg-[var(--background-principal)]">
-      <header>
-          <Navbar onLoginClick={() => setActiveModal('login')}
-          onRegisterClick={() => setActiveModal('register')}/>
-      </header>
 
 
       <main className="flex-1 bg-[var(--blanco)] pt-[4.5rem] px-4 md:px-12">
@@ -375,13 +368,6 @@ export default function Terminos() {
           Términos y condiciones
         </a>*/}
       </footer>
-      {activeModal === 'login' && (
-        <LoginModal onClose={() => setActiveModal(null)} onRegisterClick={() =>setActiveModal('register')} onPasswordRecoveryClick={() => console.log('Recuperar contraseña')}/>
-      )}
-      
-      {activeModal === 'register' && (
-        <RegisterModal onClose={() => setActiveModal(null)} onLoginClick={() => setActiveModal('login')}/>
-      )}
     </div>
   );
 }
