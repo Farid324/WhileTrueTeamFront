9e65755d3a83a8ab8496c8382f4d7cd0fb75ff75
MaiteSuarez <suarezmaite355@gmail.com>
2025-04-28 21:36:05 -0400

    Mejorar accesibilidad y estructura del botón de registro con Google

--- my-app/src/app/components/auth/authregistro/RegisterModal.tsx
+++ my-app/src/app/components/auth/authregistro/RegisterModal.tsx
@@ -386,13 +386,15 @@ export default function RegisterModal({
 
             {/* campo registro con google */}
             <div className={styles.googleBtn}>
-              <button type="button" onClick={handleGoogleRegister}>
+              <button type="button" onClick={handleGoogleRegister}aria-label="Registrarse con Google">
                 <span className={styles.googleIcon}>
                   {/* Logo de Google */}
                   <svg
                     xmlns="http://www.w3.org/2000/svg"
                     viewBox="-3 0 262 262"
                     preserveAspectRatio="xMidYMid meet"
+                    role="img"
+                    aria-hidden="true"
                   >
                     <path
                       d="M255.878 133.451c0-10.734-.871-18.567-2.756-26.69H130.55v48.448h71.947c-1.45 12.04-9.283 30.172-26.69 42.356l-.244 1.622 38.755 30.023 2.685.268c24.659-22.774 38.875-56.282 38.875-96.027"
