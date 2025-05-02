601d1477453e1aa4a5a5a9055767d66d5e8d9748
Farid <geelgaga@gmail.com>
2025-05-01 23:36:15 -0400

    Boton inicio de sesion con google

--- my-app/src/app/components/auth/authInicioSesion/LoginModal.tsx
+++ my-app/src/app/components/auth/authInicioSesion/LoginModal.tsx
@@ -130,7 +130,9 @@ export default function LoginModal({ onClose, onRegisterClick, onPasswordRecover
 
   return (
     <div className="fixed inset-0 flex justify-center items-center z-[9999] bg-black/20">
-      <div className="w-[34rem] h-auto shadow-[0_0px_20px_rgba(0,0,0,0.72)] p-10 rounded-[35px] bg-[var(--blanco)]">
+      <div className="w-full h-full  
+      p-10 bg-[var(--blanco)] 
+      sm:h-auto sm:w-[34rem] sm:rounded-[35px] sm:shadow-[0_0px_20px_rgba(0,0,0,0.72)]">
         <svg
           viewBox="0 0 1024 1024"
           xmlns="http://www.w3.org/2000/svg"
@@ -155,6 +157,26 @@ export default function LoginModal({ onClose, onRegisterClick, onPasswordRecover
             Iniciar sesión
           </span>
         </h1>
+        <div className='flex justify-center mb-6 font-bold'>
+        <button className='w-64 flex py-2 shadow-[2px_2px_4px_rgba(0,0,0,0.4)] border-2 border-[var(--negro)] rounded-lg cursor-pointer 
+        transition-all duration-150
+        active:scale-95 active:shadow-inner
+        hover:shadow-md'>
+          <svg xmlns="http://www.w3.org/2000/svg"  
+          viewBox="0 0 48 48" 
+          width="24px" 
+          height="24px"
+          className='mx-3'>
+          <path fill="#fbc02d" d="M43.611,20.083H42V20H24v8h11.303c-1.649,4.657-6.08,8-11.303,8c-6.627,0-12-5.373-12-12	s5.373-12,12-12c3.059,0,5.842,1.154,7.961,3.039l5.657-5.657C34.046,6.053,29.268,4,24,4C12.955,4,4,12.955,4,24s8.955,20,20,20	s20-8.955,20-20C44,22.659,43.862,21.35,43.611,20.083z"/>
+          <path fill="#e53935" d="M6.306,14.691l6.571,4.819C14.655,15.108,18.961,12,24,12c3.059,0,5.842,1.154,7.961,3.039	l5.657-5.657C34.046,6.053,29.268,4,24,4C16.318,4,9.656,8.337,6.306,14.691z"/>
+          <path fill="#4caf50" d="M24,44c5.166,0,9.86-1.977,13.409-5.192l-6.19-5.238C29.211,35.091,26.715,36,24,36	c-5.202,0-9.619-3.317-11.283-7.946l-6.522,5.025C9.505,39.556,16.227,44,24,44z"/>
+          <path fill="#1565c0" d="M43.611,20.083L43.595,20L42,20H24v8h11.303c-0.792,2.237-2.231,4.166-4.087,5.571	c0.001-0.001,0.002-0.001,0.003-0.002l6.19,5.238C36.971,39.205,44,34,44,24C44,22.659,43.862,21.35,43.611,20.083z"/>
+          </svg>
+          <h4 className='text-[var(--azul-oscuro)] ms-10'>
+            Iniciar sesión
+          </h4>
+        </button>
+        </div>
         {/*borde correo*/}
         <div className={`flex shadow-[2px_2px_4px_rgba(0,0,0,0.4)] mt-0 rounded-lg border-2 border-solid ${hasLoginError ? 'border-[var(--rojo)]' : 'border-[var(--negro)]'}`}>
           {/*icono correo*/}
