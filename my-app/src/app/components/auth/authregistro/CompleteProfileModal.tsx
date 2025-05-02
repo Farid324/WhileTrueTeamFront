4a55d21c6bb1b40f44668ab16e6f0e5fbc0277e1
Carla Villarroel <aslecarmy@gmail.com>
2025-05-02 17:47:05 -0400

    Agrege campo telefono

--- my-app/src/app/components/auth/authregistro/CompleteProfileModal.tsx
+++ my-app/src/app/components/auth/authregistro/CompleteProfileModal.tsx
@@ -15,21 +15,10 @@ export default function CompleteProfileModal({
   const [birthDay, setBirthDay] = useState("");
   const [birthMonth, setBirthMonth] = useState("");
   const [birthYear, setBirthYear] = useState("");
-  const [error, setError] = useState("");
-  const userEmail = localStorage.getItem("google_email");
+  const [phoneValue, setPhoneValue] = useState("");
 
   const handleSubmit = async (e: React.FormEvent) => {
     e.preventDefault();
-
-    if (!name.trim()) {
-      setError("El nombre es obligatorio");
-      return;
-    }
-
-    if (!birthDay || !birthMonth || !birthYear) {
-      setError("Completa la fecha de nacimiento");
-      return;
-    }
 
     const birthDate = new Date(
       Number(birthYear),
@@ -37,79 +26,17 @@ export default function CompleteProfileModal({
       Number(birthDay)
     );
 
-    if (birthDate > new Date()) {
-      setError("La fecha de nacimiento no puede ser futura");
-      return;
+    onComplete({ name: name.trim(), birthDate: birthDate.toISOString() });
+    if (onSuccess) {
+      onSuccess();
     }
-
-    const today = new Date();
-    let age = today.getFullYear() - birthDate.getFullYear();
-    const m = today.getMonth() - birthDate.getMonth();
-    if (m < 0 || (m === 0 && today.getDate() < birthDate.getDate())) {
-      age--;
-    }
-
-    if (age < 18) {
-      setError("Debes tener al menos 18 años");
-      return;
-    } else if (age > 85) {
-      setError("La edad máxima permitida es de 85 años");
-      return;
-    }
-    setError("");
-
-    try {
-      const res = await fetch("http://localhost:3001/api/update-profile", {
-        method: "PATCH",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        credentials: "include",
-        body: JSON.stringify({
-          email: userEmail,
-          nombre_completo: name.trim(),
-          fecha_nacimiento: birthDate.toISOString(),
-        }),
-      });
-
-      if (!res.ok) {
-        const data = await res.json();
-
-        
-        if (data.message?.includes("registrado con email")) {
-            alert("Esta cuenta ya fue registrada con correo y contraseña. Por favor inicia sesión manualmente.");
-            return; // No continuar ni cerrar el modal
-          }
-        
-          throw new Error(data.message || "No se pudo actualizar el perfil");
-      }
-
-      onComplete({ name: name.trim(), birthDate: birthDate.toISOString() });
-
-      if (onSuccess) {
-        onSuccess(); // ✅ activa el modal de éxito
-      }
-    } catch (err) {
-      console.error("Error al guardar datos de perfil", err);
-      setError("No se pudo guardar los datos. Intenta nuevamente.");
-    }
-
-    /*  onComplete({
-      name: name.trim(),
-      birthDate: birthDate.toISOString(),
-    });
-
-    if (onSuccess) onSuccess();
-    onClose(); */
   };
 
   return (
     <div className={styles.overlay}>
       <div className={styles.modal}>
         <h2 className={styles.title}>REGISTRARSE</h2>
-        <p
-          style={{ textAlign: "center", marginBottom: "1rem", fontWeight: 500, color: "blue" }}
-        >
+        <p style={{ textAlign: "center", marginBottom: "1rem", fontWeight: 500, color: "blue" }}>
           ¡Ya casi acabas!
         </p>
 
@@ -123,85 +50,66 @@ export default function CompleteProfileModal({
               name="name"
               value={name}
               placeholder="Ingrese su nombre completo"
-              onChange={(e) => {
-                const input = e.target.value;
-                const regex = /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]*$/; // permite letras y espacios
-                if (regex.test(input) || input === "") {
-                  setName(input);
-                }
-              }}
-              pattern="[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+"
-              title="Solo se permiten letras y espacios"
+              onChange={(e) => setName(e.target.value)}
               className={styles.input}
               required
             />
           </div>
 
-      
-
           {/* Fecha de nacimiento */}
           <div className={styles.halfInput}>
             <label>Fecha de nacimiento</label>
             <div className={styles.birthInputs}>
-              <select
-                value={birthDay}
-                onChange={(e) => setBirthDay(e.target.value)}
-                className={styles.select}
-              >
+              <select value={birthDay} onChange={(e) => setBirthDay(e.target.value)} className={styles.select}>
                 <option value="">DD</option>
                 {[...Array(31)].map((_, i) => (
-                  <option key={i + 1} value={i + 1}>
-                    {i + 1}
-                  </option>
+                  <option key={i + 1} value={i + 1}>{i + 1}</option>
                 ))}
               </select>
-              <select
-                value={birthMonth}
-                onChange={(e) => setBirthMonth(e.target.value)}
-                className={styles.select}
-              >
+              <select value={birthMonth} onChange={(e) => setBirthMonth(e.target.value)} className={styles.select}>
                 <option value="">MM</option>
                 {[...Array(12)].map((_, i) => (
-                  <option key={i + 1} value={i + 1}>
-                    {i + 1}
-                  </option>
+                  <option key={i + 1} value={i + 1}>{i + 1}</option>
                 ))}
               </select>
-              <select
-                value={birthYear}
-                onChange={(e) => setBirthYear(e.target.value)}
-                className={styles.select}
-              >
+              <select value={birthYear} onChange={(e) => setBirthYear(e.target.value)} className={styles.select}>
                 <option value="">AAAA</option>
                 {[...Array(100)].map((_, i) => {
                   const year = new Date().getFullYear() - i;
-                  return (
-                    <option key={year} value={year}>
-                      {year}
-                    </option>
-                  );
+                  return <option key={year} value={year}>{year}</option>;
                 })}
               </select>
             </div>
           </div>
 
-          {error && (
-            <p
-              style={{ color: "red", fontSize: "0.75rem", marginTop: "0.5rem" }}
-            >
-              {error}
-            </p>
-          )}
+          {/* Teléfono */}
+          <div className={styles.halfInput}>
+            <label htmlFor="phone">Teléfono</label>
+            <div className={styles.phoneWrapper}>
+              <span className={styles.prefix}>+591</span>
+              <input
+                type="tel"
+                id="phone"
+                name="phone"
+                value={phoneValue}
+                onChange={(e) => setPhoneValue(e.target.value)}
+                maxLength={8}
+                inputMode="numeric"
+                pattern="[0-9]*"
+                placeholder="Ingrese número de teléfono"
+                className={styles.input3}
+              />
+            </div>
+          </div>
 
           <button type="submit" className={styles.button}>
             ¡Registrarme!
           </button>
         </form>
 
-        <button className={styles.close} onClick={onClose}>
-          ✕
-        </button>
+        <button className={styles.close} onClick={onClose}>✕</button>
       </div>
     </div>
   );
 }
+
