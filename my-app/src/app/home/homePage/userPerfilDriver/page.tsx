0536e110f910b9baae6f754a30d4c22097d17a9c
Alan <ferchoalan81@gmail.com>
2025-05-09 14:29:14 -0400

    Crear el navbar de la pagina

---
+++ my-app/src/app/home/homePage/userPerfilDriver/page.tsx
@@ -0,0 +1,10 @@
+import NavbarPerfilUsuario from "@/app/components/navbar/NavbarPerfilUsuario";
+
+export default function Page() {
+  return (
+    <>
+      <NavbarPerfilUsuario />
+      <div className="min-h-screen bg-white"></div>
+    </>
+  );
+}
