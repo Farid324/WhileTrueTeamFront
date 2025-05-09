a38fef90efa2197a837938a51f1c296754bdf04a
Alan <ferchoalan81@gmail.com>
2025-05-09 15:00:58 -0400

    Agregar campo de foto de perfil driver

--- my-app/src/app/home/homePage/userPerfilDriver/UserPerfilDriver.tsx
+++ my-app/src/app/home/homePage/userPerfilDriver/UserPerfilDriver.tsx
@@ -1,10 +1,19 @@
+"use client";
+import React from "react";
 import NavbarPerfilUsuario from "@/app/components/navbar/NavbarPerfilUsuario";
+import PerfilIcon from "@/app/components/Icons/Perfil";
 
-export default function Page() {
+export default function UserPerfilDriver() {
   return (
     <>
       <NavbarPerfilUsuario />
-      <div className="min-h-screen bg-white"></div>
+      <div className="border-b border-gray-300" />
+
+      <main className="min-h-screen bg-white flex items-start justify-start px-16 py-16">
+        <div className="w-36 h-36 bg-gray-200 rounded-2xl flex items-center justify-center">
+          <PerfilIcon className="w-20 h-20 text-black" />
+        </div>
+      </main>
     </>
   );
 }
